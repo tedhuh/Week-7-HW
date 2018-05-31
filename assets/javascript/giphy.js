850bbe425c282eeae2cd4a031b6df8eb60fec347
ted huh <tedhuh@teds-MacBook-Pro.local>
2018-05-31 16:55:37 -0700

    repush

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,47 +1,53 @@
 var topics = ["baskeball", "baseball", "football"]
 
+
 function createGifButton(){
-    var buttonId = 0
     $("#made-button").on("click", function(event){
     event.preventDefault();
-    var gifButton = $("#forms").val()
-    gifButton = buttonElem;
-    var buttonElem = $("<button>").text(gifButton);
-    buttonElem.attr("id", "data-sport" + buttonId)
+      var gifButton = $("#forms").val()
+      var buttonElem = $("<button>")
+    buttonElem.text(gifButton);
+    var input = $("#forms").val().trim();
+    buttonElem.attr("id", input);
     $(".buttons").append(buttonElem);
-    buttonId++;
-    })
-    }
+    createGif();
+      })
+  }
     createGifButton();
   
   
-  
-  $("button").on("click", function(){
-    var sportButton = $(this).attr("data-sport");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sportButton + "&api_key=eq7r86RZgKxPYHDQbFgbAa1JxUdRNf5J&limit=10"
+function createGif(){
+ $("button").on("click", function(){
+  $("#gif-images").empty();
+    var input = $(this).attr("id") 
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="  + input +  "&api_key=eq7r86RZgKxPYHDQbFgbAa1JxUdRNf5J&limit=10"
     axios.get(queryURL) 
     .then(function(response){
       var results = response.data.data;
         for(var i = 0; i < results.length; i++) {
+            var sportRating = $("<p>");
+            sportRating.text("Rating: " + results[i].rating);
+            $("#gif-images").append(sportRating);
             var sportImage = $("<img>");
             sportImage.attr("src", results[i].images.downsized.url);
             sportImage.attr("data-state");
-            $("#gif-images").append(sportImage) 
-            console.log(response.data)
-        }
-    $("img").on("click", function(){
-      var state = $(this).attr("data-state");
-      var still = $(this).attr("data-still");
-      if (state === still){
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-          console.log(state);
-      }
-    });
-  
-    
+            $("#gif-images").append(sportImage)
+            console.log(response.data.data)
+          }
       })
     })
+  }
+
+
+
+for(var j = 0; j < topics.length; j++){
+  var initButton = $("<button>");
+  initButton.text(topics[j]);
+  initButton.attr("id", topics[j]);
+  $(".buttons").append(initButton);
+  createGif();
+}
+
+
+
+
